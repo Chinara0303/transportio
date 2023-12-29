2ae1f9b49e7f395c67c8ecf2713a30c59f2cc5d0
Chinara0303 <chinaraei@code.edu.az>
2023-12-29 23:01:19 +0400

    Feat: Added service detail [age

---
+++ assets/js/detail.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let image = document.querySelector('#detail .left .img img')
+let icon = document.querySelector('#detail .left .icon img')
+let titles = document.querySelectorAll('.service-name')
+let content = document.querySelector('.left .content')
+let video = document.querySelector('.right .content .video-btn')
+
+let service = JSON.parse(localStorage.getItem("clickedService"))
+
+image.setAttribute("src", service.img);
+for (const title of titles) {
+    title.innerHTML = service.name;
+}
+
+content.innerHTML = service.description;
+icon.setAttribute('src', service.icon);
+video.setAttribute('href', service.video);
+
+let servicesList = document.querySelectorAll('.right .sidebar-services-list li');
+
+for (const serviceList of servicesList) {
+    if(serviceList.id == service.id){
+        serviceList.classList.add('active')
+    }
+}
